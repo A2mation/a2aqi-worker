c14f1e85dc2423b9cdae5701055675104a50138e
Ayan <aqiindia.a2mation@gmail.com>
2026-02-13 13:37:45 +0530

    feat: implement sensor job worker with hourly and daily aggregation logic, and define SensorJob type

--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,123 @@ import { prisma } from './libs/prisma'
 import { connection } from "./libs/redis";
 import { roundToDay } from "./utils/roundToDay";
 import { roundToHour } from "./utils/roundToHour";
+import { SensorJob } from "../types/sensor";
 
 
-type SensorJob = {
-    deviceId: string;
-    timestamp: string;
 
-    aqi: number;
-    pm25?: number;
-    pm10?: number;
-    no2?: number;
-    so2?: number;
-    o3?: number;
-    co?: number;
+const worker = new Worker(
+    "sensor-queue",
+    async (job) => {
+        const data = job.data as SensorJob;
 
-    temperature?: number;
-    humidity?: number;
-};
+        const deviceId = data.deviceId;
+        const timestamp = new Date(data.measuredAt);
 
-const worker = new Worker("sensor-queue", async (job) => {  
-    
- }, {
-    connection,
-});
+        const hourStart = roundToHour(timestamp);
+        const dayStart = roundToDay(timestamp);
+
+
+        const incFields: Record<string, number> = { count: 1 }
+        const minFields: Record<string, number> = {};
+        const maxFields: Record<string, number> = {};
+
+        function addField(value: number | undefined, key: string) {
+            if (value === undefined) {
+                return;
+            }
+            incFields[`sum${key}`] = value;
+            minFields[`min${key}`] = value;
+            maxFields[`max${key}`] = value;
+        }
+
+        addField(data.aqi, "Aqi");
+        addField(data.pm10, "Pm10");
+        addField(data.pm25, "Pm25");
+        addField(data.so2, "So2");
+        addField(data.no2, "No2");
+        addField(data.co2, "Co2");
+        addField(data.co, "Co");
+        addField(data.o3, "O3");
+        addField(data.noise, "Noise");
+        addField(data.pm1, "PM1");
+        addField(data.tvoc, "Tvoc");
+        addField(data.smoke, "Smoke");
+        addField(data.methane, "Methane");
+        addField(data.h2, "H2");
+        addField(data.ammonia, "Ammonia");
+        addField(data.h2s, "H2s");
+
+        addField(data.temperature, "Temperature");
+        addField(data.humidity, "Humidity");
+
+        // ----------------------------- 
+        //      HOURLY Aggregation 
+        // -----------------------------
+
+        await prisma.$runCommandRaw({
+            update: "HourlyAggregateReading",
+            updates: [
+                {
+                    q: {
+                        deviceId,
+                        hourStart: { $date: hourStart.toISOString() }
+                    },
+                    u: {
+                        $setOnInsert: {
+                            deviceId,
+                            hourStart: { $date: hourStart.toISOString() },
+                            createdAt: { $date: new Date().toISOString() },
+                        },
+                        $inc: incFields,
+                        ...(Object.keys(minFields).length > 0 ? {
+                            $min: minFields
+                        } : {}),
+
+                        ...(Object.keys(maxFields).length > 0 ? {
+                            $max: maxFields
+                        } : {}),
+                    },
+                    upsert: true,
+                },
+            ]
+        });
+
+
+        // -----------------------------
+        // DAILY Aggregation
+        // -----------------------------
+        
+        await prisma.$runCommandRaw({
+            update: "DailyAggregateReading",
+            updates: [
+                {
+                    q: {
+                        deviceId, dayStart
+                    },
+                    u: {
+                        $setOnInsert: {
+                            deviceId,
+                            dayStart: { $date: dayStart.toISOString() },
+                            createdAt: { $date: new Date().toISOString() },
+                        },
+                        $inc: incFields,
+                        ...(Object.keys(minFields).length > 0 ? { $min: minFields } : {}),
+                        ...(Object.keys(maxFields).length > 0 ? { $max: maxFields } : {}),
+                    },
+                    upsert: true,
+                },
+            ],
+        });
+
+
+
+        return { ok: true };
+    },
+    {
+        connection,
+        concurrency: 20,
+    }
+);
 
 worker.on("completed", (job) => {
     console.log(`✅ Job completed: ${job.id}`);
---
+++ types/sensor.d.ts
@@ -0,0 +1,22 @@
+export type SensorJob = {
+    deviceId: string;
+    measuredAt: Date;
+    aqi?: number;
+    pm10?: number;
+    pm25?: number;
+    so2?: number;
+    no2?: number;
+    co2?: number;
+    co?: number;
+    o3?: number;
+    noise?: number;
+    pm1?: number;
+    tvoc?: number;
+    smoke?: number;
+    methane?: number;
+    h2?: number;
+    ammonia?: number;
+    h2s?: number;
+    temperature?: number;
+    humidity?: number;
+};
